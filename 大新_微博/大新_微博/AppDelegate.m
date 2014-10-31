cdd3e997904f4c9287d2e3811f71094c93dde407
MaiDouDou's APP <490892495@qq.com>
2014-10-31 17:40:30 +0800

    maidoudou change

--- 大新_微博/大新_微博/AppDelegate.m
+++ 大新_微博/大新_微博/AppDelegate.m
@@ -14,6 +14,9 @@
 {
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
+    /**
+     *  MaiDouDou change
+     */
     self.window.backgroundColor = [UIColor whiteColor];
     [self.window makeKeyAndVisible];
     return YES;
