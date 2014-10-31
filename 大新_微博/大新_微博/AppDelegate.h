3e508199319492b0b15a86b7064e012f35f91048
JosonJu <88956659@21cn.com>
2014-10-31 17:28:48 +0800

    Joson

---
+++ 大新_微博/大新_微博/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  大新_微博
+//
+//  Created by JosonJu on 14/10/31.
+//  Copyright (c) 2014年 joson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ 大新_微博/大新_微博/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  大新_微博
+//
+//  Created by JosonJu on 14/10/31.
+//  Copyright (c) 2014年 joson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
