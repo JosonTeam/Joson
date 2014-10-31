ca2b94669f045cd49bc806fc896aab8a7b1c845d
JosonJu <88956659@21cn.com>
2014-10-31 16:57:55 +0800

    test

---
+++ dfgdfg/dfgdfg/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  dfgdfg
+//
+//  Created by JosonJu on 14/10/31.
+//  Copyright (c) 2014年 joson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
