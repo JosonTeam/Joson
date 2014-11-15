f932ce9a8c3e5ebb997f4d5f8adf2608a3a4c765
DevilCryed <503170221@qq.com>
2014-11-15 13:47:33 +0800

    cg
    cg

--- 大新_微博/LimitViewController.h
+++
@@ -1,9 +0,0 @@
-#import <UIKit/UIKit.h>
-
-@class EditViewController;
-
-@interface LimitViewController : UIViewController < UITableViewDataSource , UITableViewDelegate >
-
-@property (nonatomic,strong)EditViewController * edit; //编辑界面
-
-@end
